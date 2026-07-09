0f8e1352193e8f01accceb4bb8ffffd7dd4eb07a
Pavel Zwerschke <pavelzw@gmail.com>
2026-07-09 22:39:26 +0200

    Update build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,7 +2,10 @@
 
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
-set RUSTFLAGS=%RUSTFLAGS% --cfg auditable
+
+:: Remove this wrapper once https://github.com/conda-forge/rust-activation-feedstock/pull/79 is merged
+copy "%RECIPE_DIR%\cargo-auditable-wrapper.bat" "%BUILD_PREFIX%\bin\cargo-auditable-wrapper.bat" || exit 1
+set CARGO=cargo-auditable-wrapper
 
 :: Bundle licenses
 cargo-bundle-licenses --format yaml --output "%SRC_DIR%\THIRDPARTY.yml" || exit 1
